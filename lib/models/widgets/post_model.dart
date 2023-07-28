b93d2aa4d17c4341ce00c44fb26e57ec613e18ab
AmirsepehrAbedini <amirsepehrabedini@gmail.com>
2023-07-28 13:04:33 +0330

    add postmodel

---
+++ lib/models/widgets/post_model.dart
@@ -0,0 +1,33 @@
+class PostModel{
+  final String? postImage;
+  final String? postTitle;
+  final String? postDescription;
+  final String? postDate;
+  final String? postTime;
+  final int? postLikes;
+  final int? postComments;
+  final int? postShares;
+  final int? postViews;
+  final String? postAuthor;
+  final String? postAuthorImage;
+  final String? postAuthorDescription;
+  final String? postId;
+
+
+  PostModel({
+     this.postImage,
+     this.postTitle,
+     this.postDescription,
+     this.postDate,
+     this.postTime,
+     this.postLikes,
+     this.postComments,
+     this.postShares,
+     this.postViews,
+     this.postAuthor,
+     this.postAuthorImage,
+     this.postAuthorDescription,
+     this.postId,
+  });
+
+}
